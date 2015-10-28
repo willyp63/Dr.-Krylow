9c79150717fbb73d2e42e8f3343568624c5287d9
Wil Pirino <wilpirino@Wils-MacBook-Pro.local>
2015-10-28 16:54:03 -0400

    Initial Commit

---
+++ Dr. Krylow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dr. Krylow
+//
+//  Created by Wil Pirino on 10/28/15.
+//  Copyright (c) 2015 My Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
